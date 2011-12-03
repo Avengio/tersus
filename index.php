38fb604b5be09a2f71834d5cb2f104b38b3cf243
Beau Calvez <beau@calvez.ca>
2011-12-02 16:21:44 -0800

    made the blog post entry wider and added some content buckets above the blog post

--- index.php
+++ index.php
@@ -7,7 +7,44 @@
 
 <?php get_header(); ?>
 
+
 <section id="content">
+<!-- Question buhkets. Wherez ma buhket!? -->
+<section class="line">
+	<article class="unit size1of3">
+		<?php 
+			$page_id = 73; // 123 should be replaced with a specific Page's id from your site, which you can find by mousing over the link to edit that Page on the Manage Pages admin page. The id will be embedded in the query string of the URL, e.g. page.php?action=edit&post=123.
+			$page_data = get_page( $page_id ); // You must pass in a variable to the get_page function. If you pass in a value (e.g. get_page ( 123 ); ), WordPress will generate an error. 
+		
+			$content = apply_filters('the_content', $page_data->post_content); // Get Content and retain Wordpress filters such as paragraph tags. Origin from: http://wordpress.org/support/topic/get_pagepost-and-no-paragraphs-problem
+			$title = $page_data->post_title; // Get title
+		?>
+		<h3><?php echo $title; // Output title?></h3>
+		<?php echo $content; // Output Content ?>
+	</article>
+	<article class="unit size1of3">
+		<?php 
+			$page_id = 73; // 123 should be replaced with a specific Page's id from your site, which you can find by mousing over the link to edit that Page on the Manage Pages admin page. The id will be embedded in the query string of the URL, e.g. page.php?action=edit&post=123.
+			$page_data = get_page( $page_id ); // You must pass in a variable to the get_page function. If you pass in a value (e.g. get_page ( 123 ); ), WordPress will generate an error. 
+		
+			$content = apply_filters('the_content', $page_data->post_content); // Get Content and retain Wordpress filters such as paragraph tags. Origin from: http://wordpress.org/support/topic/get_pagepost-and-no-paragraphs-problem
+			$title = $page_data->post_title; // Get title
+		?>
+		<h3><?php echo $title; // Output title?></h3>
+		<?php echo $content; // Output Content ?>
+	</article>
+	<article class="unit size1of3">
+		<?php 
+			$page_id = 73; // 123 should be replaced with a specific Page's id from your site, which you can find by mousing over the link to edit that Page on the Manage Pages admin page. The id will be embedded in the query string of the URL, e.g. page.php?action=edit&post=123.
+			$page_data = get_page( $page_id ); // You must pass in a variable to the get_page function. If you pass in a value (e.g. get_page ( 123 ); ), WordPress will generate an error. 
+		
+			$content = apply_filters('the_content', $page_data->post_content); // Get Content and retain Wordpress filters such as paragraph tags. Origin from: http://wordpress.org/support/topic/get_pagepost-and-no-paragraphs-problem
+			$title = $page_data->post_title; // Get title
+		?>
+		<h3><?php echo $title; // Output title?></h3>
+		<?php echo $content; // Output Content ?>
+	</article>
+</section>
 <?php if (have_posts()) : ?>
 
 	<?php while (have_posts()) : the_post(); ?>
